36a27d14ed0c240439cf77ac5bb2d5742dd34415
Joe Luhrman <jaluhrman2018@gmail.com>
2022-10-18 19:52:12 -0400

    added InitDB() (tested and working)

--- GoServerApp/model/db_util.go
+++ GoServerApp/model/db_util.go
@@ -1,4 +1,36 @@
 package model
 
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
 // db_util.go -> contains functionality for initializing db,
 // automigrating tables, etc.
+
+var (
+	// connection to the database (can be local or remote)
+	DBConn *gorm.DB
+)
+
+// Creates/opens the database specified by the path argument.
+// If successful, then automigrates all tables. Returns error
+// if either of these operations fail. Currently not set up to
+// work with anything other than local sqlite db.
+func InitDB(path string) error {
+	// open db and panic if cannot
+	DBConn, err := gorm.Open(sqlite.Open(path))
+	if err != nil {
+		return err
+	}
+
+	// auto-migrate all tables. If DB already existed,
+	// AutoMigrate() will update the tables with any changed
+	// fields if the corresponding structs have changed.
+	err = DBConn.AutoMigrate(
+		&User{},
+		&Team{},
+		&Match{},
+	)
+	return err
+}
