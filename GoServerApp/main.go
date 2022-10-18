fc7c07b491597b4bc74ed7d854ee88bdbc1a1a4c
Joe Luhrman <jaluhrman2018@gmail.com>
2022-10-18 16:26:26 -0400

    added gin default router and starting up server

--- GoServerApp/main.go
+++ GoServerApp/main.go
@@ -1,4 +1,15 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
+const (
+	PORT = ":8080"
+)
+
 func main() {
+	// initialize gin router
+	router := gin.Default()
+
+	// run router
+	router.Run(PORT)
 }
