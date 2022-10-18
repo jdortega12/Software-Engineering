5d055ec3181a0cf0c3e173b5478dd07bce34cf89
Joe Luhrman <jaluhrman2018@gmail.com>
2022-10-18 18:57:36 -0400

    added metadata to team struct and gorm tags

--- GoServerApp/model/teams.go
+++ GoServerApp/model/teams.go
@@ -1,11 +1,22 @@
 package model
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 // teams.go -> database CRUDing for teams
 
 type team struct {
 	teamID uint
 
-	name string `gorm:"unique"`
+	name string `gorm:"unique;not null"`
 
 	// other data maybe
+
+	// metadata
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt
 }
