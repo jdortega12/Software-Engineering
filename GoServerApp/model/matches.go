419ae5792c58d76d68e42711effb9301d5c4c639
Joe Luhrman <jaluhrman2018@gmail.com>
2022-10-18 18:57:13 -0400

    added match struct and enums for planned table

--- GoServerApp/model/matches.go
+++ GoServerApp/model/matches.go
@@ -1,3 +1,51 @@
 package model
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 // matches.go -> database CRUDing for matches
+
+// enum for match type
+type matchType uint
+
+const (
+	REGULAR matchType = iota
+	PLAYOFF
+	FINAL
+)
+
+type match struct {
+	matchID  uint
+	seasonID uint
+
+	matchType matchType `gorm:"not null"`
+
+	// probably should just be the name of the stadium or whatever,
+	// could add street num/name, state, zip, etc.
+	location string `gorm:"not null"`
+
+	// date AND time
+	startTime time.Time `gorm:"not null"`
+	endTime   time.Time
+
+	homeTeamID uint `gorm:"not null"`
+	awayTeamID uint `gorm:"not null"`
+
+	homeTeamScore uint
+	awayTeamScore uint
+
+	likes    uint
+	dislikes uint
+
+	// player likes and dislikes? Not sure if those
+	// are meant to be for a match only or just on the
+	// player's profile permanently or both
+
+	// metadata
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt
+}
