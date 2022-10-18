ec45118f52cde58f6435ff6cee8ac1dba324c57a
Joe Luhrman <jaluhrman2018@gmail.com>
2022-10-18 19:51:50 -0400

    initial commit, one test for InitDB()

---
+++ GoServerApp/model/db_util_test.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"testing"
+)
+
+const (
+	TEST_DB_PATH = "../test.db"
+)
+
+// Checks that InitDB() does not return any errors.
+func TestInitDB(t *testing.T) {
+	err := InitDB(TEST_DB_PATH)
+	if err != nil {
+		t.Error(err)
+	}
+}
