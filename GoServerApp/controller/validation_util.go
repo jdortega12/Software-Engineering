02a4fc590eb1ee59f769fb899de9625010cf88a3
Joe Luhrman <jaluhrman2018@gmail.com>
2022-10-19 14:44:46 -0400

    updated comment

--- GoServerApp/controller/validation_util.go
+++ GoServerApp/controller/validation_util.go
@@ -1,3 +1,4 @@
 package controller
 
-// validation_util.go -> utilities for validating user sessions / permissions
+// validation_util.go -> utilities for validating user sessions / permissions,
+// may also interact with model when convenient
