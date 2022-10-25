da1fa1ca3a57bc1b139c9ead5dcbb8c7725630e0
Joe Luhrman <jaluhrman2018@gmail.com>
2022-10-25 13:53:11 -0400

    moved test to correct source file

--- GoServerApp/model/db_util_test.go
+++ GoServerApp/model/db_util_test.go
@@ -2,8 +2,6 @@ package model
 
 import (
 	"testing"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
 )
 
 const (
@@ -15,43 +13,5 @@ func TestInitDB(t *testing.T) {
 	err := InitDB(TEST_DB_PATH)
 	if err != nil {
 		t.Error(err)
-	}
-}
-
-// ensure InsertTeamNotification can insert a valid notification
-func TestInsertGood(t *testing.T) {
-	TDBConn, err := gorm.Open(sqlite.Open("../test.db"))
-
-	if err != nil {
-		panic(err)
-	}
-	
-	InitDBTest(TDBConn)
-	
-	m := make(map[string]string)
-	m["Message"] = "Pls Work"
-	m["SenderID"] = "1"
-	m["ReceiverID"] = "2"
-	if InsertTeamNotification(m) != 0 {
-		t.FailNow()
-	}
-}
-
-
-// ensure InsertTeamNotification can insert an invalid notification
-func TestInsertBad(t *testing.T) {
-	TDBConn, err := gorm.Open(sqlite.Open("../test.db"))
-
-	if err != nil {
-		panic(err)
-	}
-	
-	InitDBTest(TDBConn)
-	
-	m := make(map[string]string)
-	m["Message"] = "Pls Work"
-	m["SenderID"] = "1"
-	if InsertTeamNotification(m) != 1 {
-		t.FailNow()
 	}
 }
