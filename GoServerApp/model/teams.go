f81dbfa319f40019988eef52caa74603d0d6cdcc
kevinjmurray <54908085+kevinjmurray@users.noreply.github.com>
2022-10-26 18:49:11 -0400

    Final teams.go

--- GoServerApp/model/teams.go
+++ GoServerApp/model/teams.go
@@ -9,14 +9,18 @@ import (
 // teams.go -> database CRUDing for teams
 
 type Team struct {
-	TeamID uint
-
-	Name string `gorm:"unique;not null"`
-
-	// other data maybe
+	TeamID       uint
+	Name         string `gorm:"unique;not null" json:"team_name"`
+	TeamLocation string `json:"team_location"`
+	// TeamManager uint (maybe??)
 
 	// metadata
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt
+}
+
+func CreateTeam(team *Team) error {
+	err := DBConn.Create(team).Error
+	return err
 }
