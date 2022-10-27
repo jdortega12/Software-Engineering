d659fc0ff0e3ec7a2c936577bf378d4310745d65
Joe Luhrman <jaluhrman2018@gmail.com>
2022-10-27 17:18:46 -0400

    added InitDB() bad path test

--- GoServerApp/model/db_util_test.go
+++ GoServerApp/model/db_util_test.go
@@ -8,11 +8,24 @@ const (
 	TEST_DB_PATH = "../test.db"
 )
 
-// Checks that InitDB() does not return any errors.
+// Checks that InitDB() does not return any errors when
+// given a valid path.
 func TestInitDB(t *testing.T) {
 	var err error
 	_, err = InitDB(TEST_DB_PATH)
 	if err != nil {
 		t.Error(err)
+		t.FailNow()
+	}
+}
+
+// Checks that InitDB() does return an error when the
+// path given is bad.
+func TestInitDBBadPath(t *testing.T) {
+	var err error
+	_, err = InitDB(" ")
+	if err == nil {
+		t.Error(err)
+		t.FailNow()
 	}
 }
