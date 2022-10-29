9106e9712f55c3fa31b5d6147b6cbe99795f2a00
Joe Luhrman <jaluhrman2018@gmail.com>
2022-10-28 21:24:05 -0400

    added screens for presentation

--- ReactMobileApp/App.js
+++ ReactMobileApp/App.js
@@ -4,12 +4,28 @@ import HomeScreen from "./view/screen/HomeScreen";
 import UpdateUserPersonalInfoScreen from "./view/screen/UpdateUserPersonalInfo";
 import CreateAccount from "./view/screen/CreateAccountScreen"
 import Login from "./view/screen/LoginScreen"
+import CreateTeam from "./view/screen/CreateTeam"
+//import UploadImage from "./view/screen/UploadImage"
+import AskManagerRequestForm from "./view/component/AskManagerRequestForm"
+import InvitePlayerRequestForm from "./view/component/InvitePlayerRequestForm"
+import TeamRequestForm from "./view/component/TeamRequestForm"
 
+// The plan for the acceptance test presentation is to 
+// manually switch screens becuase we do not yet have the 
+// navigation bar working.
 export default function App(){
     return (
-        //<HomeScreen />
+        <CreateAccount />
+        //<Login />
+        
         //<UpdateUserPersonalInfoScreen />
-        //<CreateAccount />
-        <Login />
+        //<HomeScreen /> // has logout button on it for demonstration
+
+        //<CreateTeam />
+        
+        //<AskManagerRequestForm />
+        //<InvitePlayerRequestForm />
+        //<TeamRequestForm />
+        //<UploadImage />
     );
 }
