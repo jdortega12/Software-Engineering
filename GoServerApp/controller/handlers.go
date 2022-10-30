905abd573046320e2b1e0db21282a410f4afb9a2
Joe Luhrman <jaluhrman2018@gmail.com>
2022-10-30 18:03:38 -0400

    separated middleware into its own file

--- GoServerApp/controller/handlers.go
+++ GoServerApp/controller/handlers.go
@@ -15,53 +15,6 @@ const (
 
 // handlers.go -> endpoint setup and funcs bound to them
 
-// Middleware for authenticating a user. Responds with HTTP Status Unauthorized
-// if session not set or user cannot be authenticated. Sets user in context with
-// USER_KEY to be accessed by next handlers in chain.
-func userAuthMiddleware(ctx *gin.Context) {
-	username, password, sessionExists := getSessionUser(ctx)
-	if !sessionExists {
-		ctx.AbortWithStatus(http.StatusUnauthorized)
-		return
-	}
-
-	user, err := model.ValidateUser(username, password)
-	if err != nil {
-		ctx.AbortWithStatus(http.StatusUnauthorized)
-		return
-	}
-
-	ctx.Set("user", user)
-
-	ctx.Next()
-}
-
-// Middleware for authenticating that a user is a manager. Aborts with
-// HTTP Status Unauthorized if user is not manager.
-func managerAuthMiddleware(ctx *gin.Context) {
-	user := ctx.Keys[USER_KEY].(*model.User)
-
-	if user.Role != model.MANAGER {
-		ctx.AbortWithStatus(http.StatusUnauthorized)
-		return
-	}
-
-	ctx.Next()
-}
-
-// Middleware for authenticating that a user is an admin. Aborts with
-// HTTP Status Unauthorized if user is not admin.
-func adminAuthMiddleware(ctx *gin.Context) {
-	user := ctx.Keys[USER_KEY].(*model.User)
-
-	if user.Role != model.ADMIN {
-		ctx.AbortWithStatus(http.StatusUnauthorized)
-		return
-	}
-
-	ctx.Next()
-}
-
 // Sets up the handling functions bound to the Engine's api endpoints.
 func SetupHandlers(router *gin.Engine) {
 	router.NoRoute(func(ctx *gin.Context) {
