20a4cdd006d200ad1c266333300c3261bb8d9647
Joe Luhrman <jaluhrman2018@gmail.com>
2022-11-11 23:13:10 -0500

    Reverting back to merge because it don't work
    This reverts commit 77c9628a497c3a80e1855a01c91fb16b3787407e.

--- ReactMobileApp/event-handler/HandleRemovePlayer.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react-native"
-import handleRemovePlayer from "./HandleRemovePlayer"
-
-// Smoke test to make sure handleAcceptOrDeny() causes no
-// errors.
-test("handleRemovePlayer smoke test", () => {
-    handleRemovePlayer()
-})
--- ReactMobileApp/view/screen/RemovePlayer.test.js
+++
@@ -1,7 +0,0 @@
-import TestRenderer from "react-test-renderer"
-import RemovePlayer from "./RemovePlayer"
-
-test("remove player smoke test", () => {
-    const renderer = TestRenderer.create(<RemovePlayer/>)
-    const instance = renderer.root
-})
